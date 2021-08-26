fb87941fa5ca45b45c1a0cd035503ed422b7a1b1
梦宇 <xtiqin@163.com>
2021-08-26 11:30:43 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -21,17 +21,18 @@ const Main = {
     },
 
     async detect() {
-        try {
-            let r = await this._model.detect(this._player);
-        } catch (e) { }
 
-        this._context2d.clearRect(0, 0, this._overlay.width, this._overlay.height);
-        this._context2d.strokeStyle = "#0f0";
-        this._context2d.fillStyle = "#f00";
-        for (let b of r) {
-            this._context2d.strokeRect(b.bbox[0], b.bbox[1], b.bbox[2], b.bbox[3]);
-            this._context2d.fillText(`${b.class} ${b.score.toFixed(2)}`, b.bbox[0], b.bbox[1]);
-            console.debug(`${b.class} ${b.score} \nx:${b.bbox[0]} \ny:${b.bbox[1]} \nwidth:${b.bbox[2]} \nheight:${b.bbox[3]}`);
+        if (this._player.readyState == 4) {
+            let r = await this._model.detect(this._player);
+
+            this._context2d.clearRect(0, 0, this._overlay.width, this._overlay.height);
+            this._context2d.strokeStyle = "#0f0";
+            this._context2d.fillStyle = "#f00";
+            for (let b of r) {
+                this._context2d.strokeRect(b.bbox[0], b.bbox[1], b.bbox[2], b.bbox[3]);
+                this._context2d.fillText(`${b.class} ${b.score.toFixed(2)}`, b.bbox[0], b.bbox[1]);
+                console.debug(`${b.class} ${b.score} \nx:${b.bbox[0]} \ny:${b.bbox[1]} \nwidth:${b.bbox[2]} \nheight:${b.bbox[3]}`);
+            }
         }
 
         requestAnimationFrame(this._detectFunc);
